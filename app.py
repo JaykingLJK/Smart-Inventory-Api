b629ba4bfe54f34c4ce94a94b762adb970ba42e4
Liu Jikun <45059360+JaykingLJK@users.noreply.github.com>
2021-10-05 17:40:18 +0800

    recipes api

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from os import name
 from flask.wrappers import Response
 import mysql.connector
 from flask import Flask, request
@@ -32,7 +33,7 @@ def checkInventory():
     for (id_Storage, item_name, expiry_date, quantity) in cursor:
         # print("{} of {} expiring by {:%d %b %Y}".format(item_name, quantity, expiry_date))
         # inventory_list.append(Listing(item_name=item_name, expiry_date=expiry_date, quantity=quantity))
-        item = {'id':id_Storage,'item':item_name, "expiry_date":str(expiry_date), "quantity":quantity}
+        item = {'id':id_Storage,'item':item_name, "expiry_date":str(expiry_date), "amount":quantity}
         inventory_list.append(item)
     db.close()
     return json.dumps(inventory_list)
@@ -136,7 +137,62 @@ def deleteItem():
 
 @app.route('/listings', methods=['PUT'])
 def updateItem():
-    return 
+    data = json.loads(request.data.decode('utf-8'))
+    id = data['id']
+    item = data['item']
+    amount = data['amount']
+    date = data['expiry_date']
+    db = mysql.connector.connect(**config)
+    cursor = db.cursor(buffered=True)
+    query = ("UPDATE Storage SET item_name = %s, quantity = %s, expiry_date = %s WHERE id_Storage = %s")
+    cursor.execute(query, (item, amount, date, id))
+    db.commit()
+    db.close()
+    print("Updated an existing item: {} of {} expiring by {:%d %b %Y}.".format(item, amount, date))
+    return data
+
+@app.route('/recipes', method=['GET'])
+def checkRecipe():
+    db = mysql.connector.connect(**config)
+    cursor = db.cursor(buffered=True)
+    recipe_list = []
+    check = ("SELECT id_Recipe, recipe_name, ingredient_1, ingredient_2, ingredient_3, ingredient_4 FROM Recipe ORDER BY id_Recipe")
+    cursor.execute(check)
+    for (id_Recipe, recipe_name, ingredient_1, ingredient_2, ingredient_3, ingredient_4) in cursor:
+        a_recipe = {'id':id_Recipe, 'name':recipe_name, 'ingredient_1':ingredient_1, 'ingredient_2':ingredient_2, 'ingredient_3':ingredient_3, 'ingredient_4':ingredient_4}
+        recipe_list.append(a_recipe)
+    db.commit()
+    db.close()
+    return json.dump(recipe_list)
+
+@app.route('/recipes', method=['POST'])
+def addRecipe():
+    data = json.loads(request.data.decode('utf-8'))
+    name = data['name']
+    ingredient_1 = data['ingredient_1']
+    ingredient_2 = data['ingredient_2']
+    ingredient_3 = data['ingredient_3']
+    ingredient_4 = data['ingredient_4']
+    db = mysql.connector.connect(**config)
+    cursor = db.cursor(buffered=True)
+    # check if the recipe exists or not
+    check = ("SELECT recipe_name FROM Recipe WHERE recipe_name = %s")
+    cursor.execute(check, (name))
+    data = cursor.fetchall()
+    if data:
+        query = ("UPDATE Recipe SET ingredient_1 = %s, ingredient_2 = %s, ingredient_3 = %s, ingredient_4 = %s WHERE recipe_name = %s")
+        cursor.execute(query, (ingredient_1, ingredient_2, ingredient_3, ingredient_4, name))
+        db.commit()
+        print("Changed an existing recipe: {} needs {}, {}, {}, {}.".format(name, ingredient_1, ingredient_2, ingredient_3, ingredient_4))
+    else:
+        query = ("INSERT INTO Recipe (recipe_name, ingredient_1, ingredient_2, ingredient_3, ingredient_4) VALUES (%s, %s, %s, %s, %s);")
+        cursor.execute(query, (name, ingredient_1, ingredient_2, ingredient_3, ingredient_4))
+        db.commit()
+        print("Added a new recipe: {} needs {}, {}, {}, {}.".format(name, ingredient_1, ingredient_2, ingredient_3, ingredient_4))
+    db.close()
+    return json.dumps(data)
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
